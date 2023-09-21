c10871e78d4bc2d31196f4c143d766cafd61acaf
Mohamed Mostafa Moharam <haramm058@gmail.com>
2023-09-22 00:40:40 +0300

    up3

--- src/components/pages/cart/Success.jsx
+++ src/components/pages/cart/Success.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect ,useState} from "react";
 import { checkout } from "../../../services/cart.service";
 import { gState } from "../../../context/Context";
 import { getAddresses } from "../../../services/address.service";

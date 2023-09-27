dce13e7bdad0b101874ae5695c4c8248b82eff69
Mohamed Mostafa Moharam <haramm058@gmail.com>
2023-09-27 16:54:39 +0300

    u14

--- src/components/Products/item/Product.jsx
+++ src/components/Products/item/Product.jsx
@@ -1,11 +1,11 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addProductToCart } from "../../../services/cart.service";
 const Product = ({ product }) => {
-  const navigate = useNavigate(); 
-  const absPath="https://chicwardrobe-znz5.onrender.com/";
+  const navigate = useNavigate();
+  const absPath = "https://chicwardrobe-znz5.onrender.com/";
   const [quantity, setQuantity] = useState(1);
-
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
   const handleAddToCart = async () => {
     try {
       await addProductToCart(product._id, product.size[1]);
@@ -20,21 +20,31 @@ const Product = ({ product }) => {
       <div className="card gy-3">
         <a>
           <img
-            src={absPath+product.image}
+            src={absPath + product.image}
             alt="Product"
             className="card-img-top p-2"
             onClick={() => navigate(`details/${product._id}`)}
-          /> 
+          />
         </a>
         <div className="card-body">
-        <div className="card-title"><h4>{product.name}</h4></div>
+          <div className="card-title">
+            <h4>{product.name}</h4>
+          </div>
           <div className="card-text">
-            <a  style={{color: "black",textDecoration: "none"}}>
-               {product.description}
+            <a style={{ color: "black", textDecoration: "none" }}>
+              {product.description}
             </a>
           </div>
           <div className="card-text">{product.price} EGP</div>
-          <button className="btn btn-dark btn-sm mt-1" style={{width:"100%"}} onClick={()=>handleAddToCart()}>Add To Cart</button>
+          {isLoggedIn && (
+            <button
+              className="btn btn-dark btn-sm mt-1"
+              style={{ width: "100%" }}
+              onClick={() => handleAddToCart()}
+            >
+              Add To Cart
+            </button>
+          )}
         </div>
       </div>
     </div>
